284476ad40998c801ee1dd62ec0a7fdcd5c7e54a
tamaina <tamaina@hotmail.co.jp>
2021-01-15 22:54:23 +0900

    Update git-push-results.sh

--- .github/workflows/git-push-results.sh
+++ .github/workflows/git-push-results.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 git fetch
 cd docs
+git add --all
+git commit -a -m "fuck the branch"
 git filter-branch -f --index-filter "git rm -rf --ignore-unmatch ./ --cached" --prune-empty
 git add --all
 git commit -a -m "auto-built from $GITHUB_SHA"
