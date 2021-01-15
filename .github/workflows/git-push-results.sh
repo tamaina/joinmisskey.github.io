133110b433551c31f45b6c039722b8e185e0cff1
tamaina <tamaina@hotmail.co.jp>
2021-01-15 23:07:15 +0900

    Update git-push-results.sh

--- .github/workflows/git-push-results.sh
+++ .github/workflows/git-push-results.sh
@@ -9,6 +9,9 @@ git commit -a -m "auto-built from $GITHUB_SHA"
 git push --force origin master
 cd ../dist
 git add --all
+git commit -a -m "fuck the branch"
+git filter-branch -f --index-filter "git rm -rf --ignore-unmatch ./ --cached" --prune-empty
+git add --all
 git commit -a -m "auto-built from $GITHUB_SHA"
 git push --force origin dist
 cd ..
