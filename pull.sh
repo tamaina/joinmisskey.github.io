ed86f6710b52041b14c65b98946a32a13ab5e9e5
tamaina <tamaina@hotmail.co.jp>
2021-01-16 04:22:09 +0900

    さすがに壊れたのでやめた

--- pull.sh
+++ pull.sh
@@ -2,9 +2,9 @@
 git fetch
 cd dist
 git add --all
-git commit -a -m %1
+git commit -a -m $1
 git pull
 cd ..
 git add --all
-git commit -a -m %1
+git commit -a -m $1
 git pull
--- push.sh
+++ push.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 git fetch
+cd dist
 git add --all
-git commit -a -m %1
+git commit -a -m $1
+git pull
+git push origin dist
+cd ..
+git add --all
+git commit -a -m $1
 git pull
 git push origin src
