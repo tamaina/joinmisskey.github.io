9dbb622e0598902a687024ca02f08f3c119e57a7
luckybeast <remu.teshigawara@gmail.com>
2021-01-15 19:22:39 +0000

    auto-built from 0d0ac6bc6ee4d9cafb258335dc64f29306c4885c

---
+++ files/images/icons/topng.bat
@@ -0,0 +1,6 @@
+"C:\Program Files\Inkscape\inkscape" -f icon-2.svg -e icon-2.png -w 700 -h 700
+magick convert icon-2.png -resize 256x256 favicon.ico
+"C:\Program Files\Inkscape\inkscape" -f icon-2.svg -e icon-2-192.png -w 192 -h 192
+"C:\Program Files\Inkscape\inkscape" -f icon-2-maskable.svg -e icon-2-maskable-1200.png -w 1200 -h 1200
+"C:\Program Files\Inkscape\inkscape" -f icon-2-maskable.svg -e icon-2-maskable-720.png -w 720 -h 720
+"C:\Program Files\Inkscape\inkscape" -f icon-2-maskable.svg -e icon-2-maskable-192.png -w 192 -h 192
