5d073720c43d2ca820f18bad991b268019f68718
tamaina <tamaina@hotmail.co.jp>
2021-02-04 18:22:56 +0900

    :v:

--- pull.bat
+++ pull.bat
@@ -1,9 +1,7 @@
 git fetch
-cd dist
-git add --all
-git commit -a -m %1
-git pull
-cd ..
+git worktree remove dist
+git branch -D dist
+git worktree add dist dist
 git add --all
 git commit -a -m %1
 git pull
--- pull.sh
+++ pull.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 git fetch
-cd dist
 git worktree remove dist
 git branch -D dist
 git worktree add dist dist
-cd ..
 git add --all
 git commit -a -m $1
 git pull
